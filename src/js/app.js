3bce2c4f7de57fe213bc572129e742975da5dc26
sarpensortia <kateryna.shepilova@gmail.com>
2022-02-17 00:43:04 +0200

    Added Portfolio Lightbox

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
 $('.slider-feedback').slick();
+
+lightGallery(document.getElementById('lightgallery'));
+
+$(function () {
+	$(".team-img").jqZoom({
+		selectorWidth: 30,
+		selectorHeight: 30,
+		viewerWidth: 400,
+		viewerHeight: 400
+	});
+})
