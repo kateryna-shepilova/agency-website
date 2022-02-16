7d104280ac4299a924670a7cb966bac6f63ac72e
Varfolomei <varfolomeisafronov@gmail.com>
2022-02-17 00:15:46 +0200

    header

--- build/js/app.js
+++ build/js/app.js
@@ -2,3 +2,12 @@
 // 	// $('body').hide()
 // })
 $('.slider-feedback').slick();
+$('.slider-banner').slick();
+
+$(document).ready(function () {
+	$(".header__burger").click(function (event) {
+		$(".header__burger,.header__menu").toggleClass("active");
+		$("body").toggleClass("lock");
+	});
+});
+
